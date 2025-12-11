7edc83072d5d014e99ba8e2365dc186c8dde73aa
santorem <santorem@students.zhaw.ch>
2025-12-11 21:35:37 +0100

    simulti docker update

--- backend/src/main/java/ch/multispace/backend/config/CorsConfig.java
+++ backend/src/main/java/ch/multispace/backend/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package ch.multispace.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,10 +10,13 @@ import org.springframework.web.cors.CorsConfigurationSource;
 @Configuration
 public class CorsConfig {
 
+    @Value("${app.frontend-url}") // read from application.yml
+    private String frontendUrl;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedOrigin(frontendUrl);
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setAllowCredentials(true);
--- backend/src/main/java/ch/multispace/backend/controllers/AuthController.java
+++ backend/src/main/java/ch/multispace/backend/controllers/AuthController.java
@@ -15,7 +15,6 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 public class AuthController {
 
     private final AuthService authService;
