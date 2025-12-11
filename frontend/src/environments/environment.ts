afaa827f56fb826a32bbdd6eee792432bca7cc9c
santorem <santorem@students.zhaw.ch>
2025-12-11 21:16:38 +0100

    simulti docker update

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://simulti.santoriello.ch/api',
+  apiUrl: 'https://simulti.santoriello.ch/api',
   wsUrl: 'ws://simulti.santoriello.ch'
 };
